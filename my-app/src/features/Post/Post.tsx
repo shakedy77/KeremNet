bff34e396797a6780c40b7912eed997bcbb5b47e
Yonatan <yonashaked77@gmail.com>
2025-06-23 17:09:24 +0300

    added time in post

--- my-app/src/features/Post/Post.tsx
+++ my-app/src/features/Post/Post.tsx
@@ -13,9 +13,11 @@ interface Props{
 }
 
 const Post = ({publisherName, content} : Props):ReactElement => {
+    let dateTime = new Date()
     return <div className="Post">
         <h2>{publisherName}</h2>
         <p>{content}</p>
+        <p>{dateTime.toDateString()} {dateTime.toLocaleTimeString()}</p>
         <Like></Like>
         <CommentExpandableList></CommentExpandableList>
         </div>
