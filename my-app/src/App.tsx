bdd4cf312e178b1f14d94c88fbf4cda7812beb98
Yonatan <yonashaked77@gmail.com>
2025-06-24 15:08:49 +0300

    added basic server communication

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import './App.css';
 
 import Page from './features/Components/Page/Page';
+import JsonData from './data/json-server/json-server';
+
+
 
 
 function App() {
+
+  console.log(JsonData())
+
   return (
     <div className="App">
       <Page></Page>
