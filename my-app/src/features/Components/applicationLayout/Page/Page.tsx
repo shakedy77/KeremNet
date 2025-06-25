2f6cd6b869a088276ff44d845f1833b0b2b5ce95
Yonatan <yonashaked77@gmail.com>
2025-06-25 11:47:46 +0300

    added logo picture to page

--- my-app/src/features/Components/applicationLayout/Page/Page.tsx
+++ my-app/src/features/Components/applicationLayout/Page/Page.tsx
@@ -1,13 +1,14 @@
 import React, { ReactElement } from "react";
 
 import LoadedPosts from "../../LoadedPosts/LoadedPosts";
+import keremLogo from "../imgs/keremNetLogo.png"
 
 import './Page.css' 
 
 const Page = ():ReactElement =>{
     return <div className="Page">
-        <title>Kerem Net</title>
-        <h1>Kerem Net</h1>
+        <title>Kerem Net </title>
+        <h1>Kerem Net <img className="logoPicture" src={keremLogo} alt="pic" /></h1>
         <div>
             <div className="PostList">
             <LoadedPosts></LoadedPosts>
