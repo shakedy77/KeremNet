b9a9daa4fb271e8cc70655d2d870846695c95ccf
Yonatan <yonashaked77@gmail.com>
2025-06-25 09:23:38 +0300

    fixed Page model and removed server

--- my-app/src/features/Components/Page/Page.tsx
+++ my-app/src/features/Components/Page/Page.tsx
@@ -4,7 +4,7 @@ import Post from "../Post/Post";
 const Page = ():ReactElement =>{
     const current_date = new Date() 
     return <div>
-        <Post publisherName={"user"} content={"test test test test test"} publishDate={current_date} likeAmount={10} comments={[{publisherName:'keremist',content:"wow!",likeAmount:3}]}></Post>
+        <Post postModel={{publisherName:"user", content:"test test test test test", publishDate:current_date, likeAmount:10, comments:[{publisherName:'keremist', content:"wow!", likeAmount:3}]}}></Post>
     </div>
 }
 
